32c5d0b0d1d37f64240a329362db29a889889992
Edward Blanciak <blanciak.edward2@gmail.com>
2020-03-27 11:33:23 -0400

    private route, axioswithauth, and login form complete

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import { TextField, Button } from '@material-ui/core';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const Login = () => {
+  const history = useHistory();
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <Formik
+      initialValues={{
+        username: '',
+        password: ''
+      }}
+      onSubmit={(data, { resetForm }) => {
+        console.log(data);
+        axiosWithAuth()
+          .post('/api/login', data)
+          .then((res) => {
+            console.log(res);
+            resetForm({});
+            history.push('/bubble-page');
+          });
+      }}
+    >
+      {({ values }) => (
+        <Form>
+          <Field
+            name="username"
+            values={values.username}
+            variant="outlined"
+            label="Username"
+            as={TextField}
+          />
+          <Field
+            name="password"
+            values={values.password}
+            variant="outlined"
+            label="Password"
+            as={TextField}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
